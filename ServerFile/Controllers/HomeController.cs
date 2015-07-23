4124a99292d2e1947b096c1c66662cd5bab1d452
tomekaq <t.antonik@sente.pl>
2015-07-23 13:16:35 +0200

    first

--- ServerFile/Controllers/HomeController.cs
+++ ServerFile/Controllers/HomeController.cs
@@ -1,8 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Mail;
 using System.Web;
 using System.Web.Mvc;
+using WebApplication1;
 
 namespace ServerFile.Controllers
 {
@@ -10,7 +13,52 @@ namespace ServerFile.Controllers
     {
         public ActionResult Index()
         {
+            Attachment();
             return View();
+        }
+
+        protected void CsGenerClick(object sender,EventArgs e) { 
+        
+        string input = Request["inputText"];
+        GenerateFile(input);
+        }
+        public void Attachment()
+        {
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("toja@wp.pl");
+            mail.To.Add("t.antonik@sente.pl");
+
+            FileStream fileContent;
+            fileContent = GenerateFile("45");
+
+            mail.Attachments.Add(new Attachment(fileContent, "Zapis111.txt", "text/plain"));
+            try
+            {
+                SmtpClient clinet = new SmtpClient("localhost");
+                clinet.Send(mail);
+            }
+            catch(Exception e)
+            {
+                int i = 0; 
+            }
+        }
+        public FileStream GenerateFile(string inputParam)
+        {
+            int k = int.Parse(inputParam);
+            using (BCRandomStream rndstream = new BCRandomStream(1000))
+            {
+                string path = @"C:\Users\user\Documents\Zapis1111.txt";
+                using (FileStream fileStream = new FileStream(path, FileMode.Create, FileAccess.ReadWrite))
+                {
+                    using (StreamWriter writeStream = new StreamWriter(fileStream))
+                    {
+                        for (var i = 0; i < k; i++)
+                            writeStream.WriteLine(rndstream.Read());
+                    }
+                    return fileStream;
+                }
+            }
         }
     }
 }
